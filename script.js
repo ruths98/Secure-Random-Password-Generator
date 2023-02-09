050e9292631c988c6c3ac2467e5b96e903cbfb9d
ruths98 <ruth91198@gmail.com>
2023-02-09 12:42:34 -0500

    add code to generate password

--- script.js
+++ script.js
@@ -1,16 +1,44 @@
 // Assignment code here
-
+let uppercase="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let lowercase="abcdefghijklmnopqrstuvwxyz";
+let numbers="1234567890";
+let specialCharacters="\"!#$%&'()*+-,/:;=[]^_{}~";
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+ let password = generatePassword();
+  let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
+}
+
+function generatePassword(){
+  let length=window.prompt("how many characters would you like your password to contain?");
+  let password="";
+  while (password.length<length) {
+    password+=lowercase.charAt(Math.floor(Math.random() * lowercase.length));
+    if (password.length==length) break;
+    password+=uppercase.charAt(Math.floor(Math.random() * uppercase.length));
+    if (password.length==length) break;
+    password+=specialCharacters.charAt(Math.floor(Math.random() * specialCharacters.length));
+    if (password.length==length) break;
+    password+=numbers.charAt(Math.floor(Math.random() * numbers.length));
+  }
+  password=shuffleWord(password);
+  return password;
+}
+
+function shuffleWord (word){
+  var shuffledWord = '';
+  word = word.split('');
+  while (word.length > 0) {
+    shuffledWord +=  word.splice(word.length * Math.random() << 0, 1);
+  }
+  return shuffledWord;
 }
 
 // Add event listener to generate button
